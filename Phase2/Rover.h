be44bc6e45c17988ee50c0149087cb3f046b0524
AhmedLotfy02 <76037906+AhmedLotfy02@users.noreply.github.com>
2021-04-24 14:53:04 +0200

    Menna Changes are added
    rovers from menna station to ahmed_L station

--- Phase2/Rover.h
+++ Phase2/Rover.h
@@ -1,11 +1,24 @@
 #pragma once
+#include "Defs.h"
 class Rover
 {
-
-private:
-
-
+	int ID;
+	int Speed;
+	int CheckupDuration;
+	M_TYPE Type;
+	int NumberOfOrderServed;
 public:
 	Rover();
+	void SetSpeed(int);
+	int GetSpeed();
+	void SetID(int);
+	int GetID();
+	void SetCheckupDuration(int);
+	int GetCheckupDuration();
+	void SetType(M_TYPE);
+	M_TYPE GetType();
+	void SetNumberOfOrderServed();
+	int GetNumberOfOrderServed();
+	~Rover();
 };
 
